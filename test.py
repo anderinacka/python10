ec53bad339ae6c9814690a58d0f42fec99386fcf
Andreas Eriksson <andreas.eriksson2@yh.nackademin.se>
2024-08-15 00:19:21 +0200

    första commit

---
+++ test.py
@@ -0,0 +1,29 @@
+S = 'first line. \nsecond line.'
+
+print(S)
+print("C:\\some\\name")
+print("C:\\some\\name")
+
+lista = """\
+Alfa
+Beta
+Delta
+"""
+
+lista2 = """Alfa
+Beta
+Delta
+"""
+
+print(lista)
+print(lista2)
+
+Hej ='hej ' + 'då'
+
+word = "andreas"
+print(word[3])
+start = 2
+slut = 6
+SLIACD = word[start:slut]
+SLICADD = word[slut]
+SLICADDD = word[start]
